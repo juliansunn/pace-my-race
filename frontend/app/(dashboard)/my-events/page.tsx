7c7ace6bc35a8403bd21f07339516fe026765a62
juliansunn <juliansunn@gmail.com>
2023-09-23 07:02:08 -0700

    adding some new page templates

---
+++ frontend/app/(dashboard)/my-events/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const MyEvents = (props: Props) => {
+	return <div>MyEvents</div>;
+};
+
+export default MyEvents;
--- frontend/components/RaceCard.tsx
+++ frontend/components/RaceCard.tsx
@@ -17,7 +17,16 @@ const RaceCard = ({ race }: Props) => {
 			key={race.id}
 			className="p-4 border-black/2 flex items-center justify-between hover:shadow-md transition cursor-pointer"
 		>
-			<h2>{race.name}</h2>
+			<div className="flex-none mr-4">
+				<img src={race.image} alt={race.name} className="w-16 h-16 rounded-full" />
+			</div>
+
+			<div className="flex flex-col justify-between">
+				<h2 className="text-xl font-bold">{race.name}</h2>
+				<div className="text-sm text-gray-600">
+					{race.participants.length} Participants
+				</div>
+			</div>
 		</Card>
 	);
 };
